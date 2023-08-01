91b8b625517a032e563f435e27e201aa51c2e0bb
Christzxz <126612073+kurays015@users.noreply.github.com>
2023-08-01 13:41:18 +0800

    Delete javascript/cleaning code directory

--- javascript/cleaning code/loader.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const showMainContent = () => {
-    loaderContainer.style.display = 'none';
-  };
-
-  setTimeout(showMainContent, 3000);
-});
